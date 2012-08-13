b4582728254098224f037b53c670dd1597275443
Jason Mooberry <jason@vimeo.com>
2012-08-13 19:10:45 -0400

    First

---
+++ README.md
@@ -0,0 +1,24 @@
+# t.js
+## A tiny javascript templating framework in ~400 bytes gzipped
+
+`t.js` is a simple solution to interpolating values in an html string for insertion into the DOM via `innerHTML`.
+
+### Features
+ * Simple interpolation: `{{=value}}`
+ * Scrubbed interpolation: `{{%unsafe_value}}`
+ * Name-spaced variables: `{{=User.address.city}}`
+ * If/else blocks: `{{value}} <<markup>> {{:value}} <<alternate markup>> {{/value}}`
+ * If not blocks: `{{!value}} <<markup>> {{/value}}`
+ * Object/Array iteration: `{{@object_value}} {{=_key}}:{{=_val}} {{/@object_value}}`
+ * Multi-line templates (no removal of newlines required to render)
+ * Render the same template multiple times with different data
+ * Works in all modern browsers
+ 
+### How to use
+
+	var template = new t("<div>Hello {{=name}}</div>");
+	document.body.innerHtml = template.render({name: "World!"});
+	
+For more advanced usage check the `t_test.html`.
+
+This software is released under the MIT license.  
---
+++ t.js
@@ -0,0 +1,99 @@
+/*
+		 _     _     
+		| |   (_)    
+		| |_   _ ___ 
+		| __| | / __|
+		| |_ _| \__ \
+		 \__(_) |___/
+		     _/ |    
+		    |__/     
+
+	t.js
+	a micro-templating framework in ~400 bytes gzipped
+
+	@author  Jason Mooberry <jasonmoo@me.com>
+	@license MIT
+	@version 0.1.0
+
+*/
+(function() {
+
+	var blockregex = /\{\{(([@!]?)(.+?))\}\}(([\s\S]+?)(\{\{:\1\}\}([\s\S]+?))?)\{\{\/\1\}\}/g,
+		valregex = /\{\{([=%])(.+?)\}\}/g, s=String;
+
+	function t(template) {
+		this.t = template;
+	}
+
+	function scrub(val) {
+		return s(val).replace(/&/g,'&amp;').replace(/>/g,'&gt;').replace(/</g,'&lt;').replace(/"/g,'&quot;');
+	}
+
+	function get_value(vars, key) {
+		var parts = key.split('.');
+		while (parts.length) {
+			if (!(parts[0] in vars)) {
+				return false;
+			}
+			vars = vars[parts.shift()];
+		}
+		return vars;
+	}
+
+	function render(fragment, vars) {
+		return fragment
+			.replace(blockregex, function(_, __, meta, key, inner, if_true, has_else, if_false) {
+				
+				var val = get_value(vars,key), temp = "", i;
+
+				if (!val) {
+
+					// handle if not
+					if (meta == '!') {
+						return render(inner, vars);
+					}
+					// check for else
+					if (has_else) {
+						return render(if_false, vars);
+					}
+
+					return "";
+				}
+
+				// regular if
+				if (!meta) {
+					return render(has_else ? if_true : inner, vars);
+				}
+				
+				// process array/obj iteration
+				if (meta == '@' && val) {
+					for (i in val) {
+						if (val.hasOwnProperty(i)) {
+							vars._key = i;
+							vars._val = val[i];
+							temp += render(inner, vars);
+						}
+					}
+					delete vars._key;
+					delete vars._val;
+					return temp;
+				}
+
+			})
+			.replace(valregex, function(_, meta, key) {
+				var val = get_value(vars,key);
+
+				if (val) {
+					return meta == '%' ? scrub(val) : val;
+				}
+				return "";
+			});
+	}
+
+	t.prototype.render = function (vars) {
+		return render(this.t, vars);
+	};
+
+	window.t = t;
+
+})();
